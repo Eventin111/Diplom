024b02ba9b09dc44abf614e584407306df3ca673
Danems003 <danilsemenov1907@yandex.ru>
2026-04-08 14:05:55 +0300

    Backend: восстановлена совместимость app.core.s3 для тестов

--- backend/app/core/s3.py
+++ backend/app/core/s3.py
@@ -1,3 +1,8 @@
-from app.infrastructure.storage.s3 import S3Client, s3_client
+from app.infrastructure.storage import s3 as _s3_impl
 
-__all__ = ["S3Client", "s3_client"]
+S3Client = _s3_impl.S3Client
+s3_client = _s3_impl.s3_client
+boto3 = _s3_impl.boto3
+settings = _s3_impl.settings
+
+__all__ = ["S3Client", "s3_client", "boto3", "settings"]
